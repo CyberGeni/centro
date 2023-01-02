4597eb817afc9312ef71bf9fdf2ab633fd602c4c
Treasure Ajefu <treasureajefu@gmail.com>
2023-01-02 15:22:47 +0100

    project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="font-Aeonik h-screen bg-black flex items-center justify-center">
+      <h1 className='text-green-600 text-xl'>project setup, now let's cook</h1>
+    </div>
+  )
+}
+
+export default App
