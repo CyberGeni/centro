bc8556be8c6bc269fb0d9325ca167ee17bfa2a5e
Treasure Ajefu <treasureajefu@gmail.com>
2023-01-03 21:51:44 +0100

    feat: hide nav on scroll

--- src/components/ExternalNavbar.jsx
+++ src/components/ExternalNavbar.jsx
@@ -1,24 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/icons/logo.svg";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import Pattern from "../assets/icons/bg-pattern.svg";
-import '../App.css'
+import "../App.css";
+import Aos from "aos";
 function ExternalNavbar() {
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
+    useClassNames: true, // if true, will add content of `data-aos` as classes on scroll
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+  });
 
-  const [isActive, setActive] = useState(false)
+  const [isActive, setActive] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
 
-  function handleClick() {  
-    isActive ? setActive(false) : setActive(true)
+    if (currentScrollPos > prevScrollPos) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
+  function handleClick() {
+    isActive ? setActive(false) : setActive(true);
     if (window.innerWidth > 768) {
-      setActive(true)
+      setActive(true);
     }
   }
 
   return (
-    // <header 
+    // <header
     //   style={{ backgroundImage:`url(${Pattern})` }}
     //   className="bg-blend-multiply font-[Manrope] flex justify-between items-center w-full bg-[#FFE6C3] px-8 sm:px-12 md:px-16 lg:px-28 xl:px-32 py-4 sm:py-6 md:py-8 h-fit"
-    // >   
+    // >
     //   <img src={Logo} alt="" />
     //   <div>
     //     <nav className="space-x-0 md:space-x-4">
@@ -27,7 +60,7 @@ function ExternalNavbar() {
     //       <Link>FAQs</Link>
     //     </nav>
     //   </div>
-      
+
     //   <div className="flex items-center space-x-6">
     //     <button className="button button--aylen px-6 py-3 font-[Manrope] bg-[#141115] hover:bg-stone-500/0.2 text-[#FFE6C3] hover:text-white relative rounded-md text-sm font-medium overflow-hidden">
     //       Connect your wallet
@@ -43,26 +76,41 @@ function ExternalNavbar() {
     //       <path fill="none" d="M0 0h24v24H0z" />
     //       <path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z" />
     //     </svg>
-    //   </div>
+    //   </div>  ${visible ? 'top-0' : ''}
     // </header>
-    <header 
-      className="px-8 sm:px-12 md:px-16 lg:px-28 xl:px-32 py-4 z-50 fixed flex justify-between mx-auto list-none overflow-hidden w-full">
-        <img src={Logo} alt="" />
-        
-        <div className=
-          {isActive ? "top-0 right-[25vw] md:left-unset transition-all flex flex-col md:flex-row justify-center w-[75vw] md:w-auto h-screen md:h-fit space-y-4 md:space-y-0 fixed md:relative bg-[#141115] text-[#FFE6C3]" : "transition-all right-[100vw] absolute md:relative md:flex md:justify-between w-full md:left-0 md:text-lg md:my-2"}>
-          <nav className="font-[Manrope] space-y-7 md:w-full md:space-y-0 flex flex-col md:flex-row md:items-center md:justify-center px-10 md:py-3 md:px-0 md:space-x-5">
-           <Link>Home</Link>
-           <Link>How it works</Link>
-           <Link>FAQs</Link>
-          </nav>
-        </div>
-        
-        <div className="flex items-center space-x-6">
-         <button className="w-full -z-10 button button--aylen px-6 py-3 font-[Manrope] bg-[#141115] hover:bg-stone-500/0.2 text-[#FFE6C3] hover:text-white relative rounded-md text-sm font-medium overflow-hidden">
-           Connect your wallet
-         </button>
-         <svg
+    <header
+      className={`transition-all px-8 sm:px-12 md:px-16 lg:px-28 xl:px-32 py-4 z-50 sticky flex justify-between mx-auto list-none overflow-hidden w-full
+        ${isActive ? " bg-[#FFE6C3]" : "backdrop-blur-md"}
+        ${visible ? "top-0 bg-[#FFE6C3]/20 transition-all" : "data-aos fade-down"}
+        `}
+    >
+      <img src={Logo} alt="" />
+
+      <div
+        className={
+          isActive
+            ? "top-0 right-[25vw] z-[1000000] md:left-unset transition-all flex flex-col md:flex-row justify-center w-[75vw] md:w-auto h-screen md:h-fit space-y-4 md:space-y-0 fixed md:relative bg-[#141115] md:bg-transparent text-[#FFE6C3] md:text-[#141115] md:right-[unset] md:space-x-4 md:items-center md:mx-auto md:justify-center"
+            : "transition-all right-[100vw] absolute md:relative md:flex md:justify-between w-full md:left-0 md:text-lg md:my-2"
+        }
+      >
+        <nav className="font-[Manrope] space-y-2 md:w-full md:space-y-0 flex flex-col md:flex-row md:items-center md:justify-center md:py-3 md:px-0 md:space-x-5">
+          <Link className="hover:bg-stone-600/30 pl-12 md:pl-0 py-5 md:py-0 hover:border-r-8 hover:border-r-[#FFE6C3] transition-all">
+            Home
+          </Link>
+          <Link className="hover:bg-stone-600/30 pl-12 md:pl-0 py-5 md:py-0 hover:border-r-8 hover:border-r-[#FFE6C3] transition-all">
+            How it works
+          </Link>
+          <Link className="hover:bg-stone-600/30 pl-12 md:pl-0 py-5 md:py-0 hover:border-r-8 hover:border-r-[#FFE6C3] transition-all">
+            FAQs
+          </Link>
+        </nav>
+      </div>
+
+      <div className="flex items-center space-x-6">
+        <button className="button button--aylen -z-2 px-6 py-3 font-[Manrope] bg-[#141115] hover:bg-stone-500/0.2 text-[#FFE6C3] hover:text-white relative rounded-md text-sm font-medium overflow-hidden w-max">
+          Connect your wallet
+        </button>
+        <svg
           onClick={handleClick}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,28 +15,7 @@ import Pattern from '../assets/icons/bg-pattern.svg'
 
 function Home() {
 
-    AOS.init({
-        // Global settings:
-        disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-        initClassName: 'aos-init', // class applied after initialization
-        animatedClassName: 'aos-animate', // class applied on animation
-        useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-        disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-        debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-        throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-        
-      
-        // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
-        delay: 0, // values from 0 to 3000, with step 50ms
-        duration: 400, // values from 0 to 3000, with step 50ms
-        easing: 'ease', // default easing for AOS animations
-        once: false, // whether animation should happen only once - while scrolling down
-        mirror: false, // whether elements should animate out while scrolling past them
-        anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-      
-      });
+    AOS.init();
 
   return (
     <div>
@@ -45,7 +24,7 @@ function Home() {
             {/* hero */}
             <div 
                 style={{ backgroundImage:`url(${Pattern})` }}
-                className="relative bg-[#FFE6C3] bg-blend-multiply h-[100vh] md:h-[100vh] flex flex-col md:flex-row justify-center md:justify-start">
+                className="relative bg-[#FFE6C3] bg-blend-multiply h-[93vh] md:h-[80vh] flex flex-col md:flex-row justify-center md:justify-start">
                 <section className="relative z-10 px-8 sm:px-12 md:px-16 lg:px-28 xl:px-32 space-y-6 md:flex md:flex-col md:justify-center md:w-4/5 lg:w-2/3 max-w-2xl md:max-w-none">
                     <h1 className="-mt-10 text-5xl sm:text-6xl md:text-7xl md:leading-none font-Aeonik font-black">Discover an easy way to make a difference</h1>
                     <p className="font-[Manrope] my-6 w-4/5 md:w-3/4 font-medium">Find worthy DAOs to make meaningful collaborations in, collaborate, and build, with Centro's DAO explorer.</p>
