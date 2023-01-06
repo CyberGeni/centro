be6e7ad7e66723252c3163c19c6dd7f3c9ff5892
Treasure Ajefu <treasureajefu@gmail.com>
2023-01-06 02:15:55 +0100

    added spacing to navlinks and removed max width of sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,28 +4,28 @@ import Logo from "../assets/icons/logo-light.svg";
 import '../App.css'
 function SideBar() {
   return (
-    <div className="bg-[#131014] h-screen w-full max-w-xs ">
+    <div className="bg-[#131014] h-screen w-full">
       <div className="py-6 mb-8">
         <img className="mx-auto" src={Logo} alt="" />
       </div>
       <nav className="text-white flex flex-col my-2">
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/all">All DAOs</NavLink>
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/clubs">Clubs</NavLink>
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/products">Products</NavLink>
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/investments">Investments</NavLink>
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/non-profits">Non-Profits</NavLink>  
         <NavLink 
-            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-4 font-[Manrope]" 
+            className="py-4 hover:border-x-[#FFE6C3] border-x-0 hover:border-x-4 hover:bg-stone-600/20 transition-all px-6 font-[Manrope]" 
             to="/dashboard/grants">Grants and Accelerators</NavLink>
       </nav>
     </div>
