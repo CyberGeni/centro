45e3ff4c26332ab480b3bf60c5aacb8005ae198e
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-01-07 23:40:34 +0100

    chore: Updated item order

--- src/db/dao.js
+++ src/db/dao.js
@@ -4,230 +4,229 @@ import dxdao from '../assets/logos/dxdao.png';
 import buzzedbear from '../assets/logos/buzzed-bear.png';
 import roadtrip from '../assets/logos/roadtripdao.png';
 import blockzero from '../assets/logos/blockzero.png';
-const daos = [   
-    {
-        id: 1,
-        image: aave,
-        name: 'Aave',
-        member_count: '120,000',
-        short_description: 'Aave is an Open Source Protocol to create Non-Custodial Liquidity Markets to earn interest on supplying and borrowing assets with a variable or stable',
-        category: 'Products',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    {
-        id: 2,
-        image: maker,
-        name: 'MakerDAO',
-        member_count: '20,000',
-        short_description: 'This is a community for those interested in the Maker token. Users can join and flex.',
-        category: 'Products',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    {
-        id: 3,
-        image: dxdao,
-        name: 'DXdao',
-        member_count: '20,000',
-        short_description: 'This is a community for those interested in the DX token. Users can join and flex.',
-        category: 'Products',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    {
-        id: 4,
-        image: buzzedbear,
-        name: 'Buzzed Bear Hideout',
-        member_count: '120',
-        short_description: 'BBH is a unique collective experimenting in web3. We are a diverse and eclectic group of NFT developers, collectors, and enthusiasts on a',
-        category: 'Social',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    {
-        id: 5,
-        image: roadtrip,
-        name: 'RoadTrip DAO',
-        member_count: '233',
-        short_description: 'RoadTrip is a decentralized community of tech. creatives; product designers, UX writers, etc. collectively supporting one another in building a successful career in the Design & Tech space.',
-        category: 'Social',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    {
-        id: 6,
-        image: blockzero,
-        name: 'BlockZero Labs',
-        member_count: '90',
-        short_description: 'Blockzero is a DAO with a mission to build, launch, and scale the next generation of Web3 startups.',
-        category: 'Investments',
-        overview: {
-            tokens: '',
-            treasury: '',
-            members: '',
-            proposals: ''
-        },
-        description: {
-            long_description: '',
-            governance: '',
-        },
-        members : [
-            {
-                id: '',
-                avatar: '',
-                username: '',
-                proposals: '',
-                percentage: ''
-            }
-            
-        ],
-        proposals: {
-            author: '',
-            topic: '',
-            description: '',
-            status: ''
-        },
-        treasury: {
-            // supposed to be a graph
-        },
-    },
-    
-    
+const daos = [
+	{
+		id: 1,
+		image: aave,
+		name: 'Aave',
+		member_count: '120,000',
+		short_description: 'Aave is an Open Source Protocol to create Non-Custodial Liquidity Markets to earn interest on supplying and borrowing assets with a variable or stable',
+		category: 'Products',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+	{
+		id: 3,
+		image: dxdao,
+		name: 'DXdao',
+		member_count: '20,000',
+		short_description: 'This is a community for those interested in the DX token. Users can join and flex.',
+		category: 'Products',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+	{
+		id: 4,
+		image: buzzedbear,
+		name: 'Buzzed Bear Hideout',
+		member_count: '120',
+		short_description: 'BBH is a unique collective experimenting in web3. We are a diverse and eclectic group of NFT developers, collectors, and enthusiasts on a',
+		category: 'Social',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+	{
+		id: 5,
+		image: roadtrip,
+		name: 'RoadTrip DAO',
+		member_count: '233',
+		short_description: 'RoadTrip is a decentralized community of tech. creatives; product designers, UX writers, etc. collectively supporting one another in building a successful career in the Design & Tech space.',
+		category: 'Social',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+	{
+		id: 6,
+		image: blockzero,
+		name: 'BlockZero Labs',
+		member_count: '90',
+		short_description: 'Blockzero is a DAO with a mission to build, launch, and scale the next generation of Web3 startups.',
+		category: 'Investments',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+	{
+		id: 2,
+		image: maker,
+		name: 'MakerDAO',
+		member_count: '20,000',
+		short_description: 'This is a community for those interested in the Maker token. Users can join and flex.',
+		category: 'Products',
+		overview: {
+			tokens: '',
+			treasury: '',
+			members: '',
+			proposals: ''
+		},
+		description: {
+			long_description: '',
+			governance: '',
+		},
+		members: [
+			{
+				id: '',
+				avatar: '',
+				username: '',
+				proposals: '',
+				percentage: ''
+			}
+
+		],
+		proposals: {
+			author: '',
+			topic: '',
+			description: '',
+			status: ''
+		},
+		treasury: {
+			// supposed to be a graph
+		},
+	},
+
 ]
 export default daos;
